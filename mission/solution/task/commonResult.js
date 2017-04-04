742e187f8266bbba191bf2ee27224de2173d0b96
tanawatdee <d.tanawat@hotmail.com>
2017-04-04 10:58:20 +0700

    add comment

---
+++ mission/solution/task/commonResult.js
@@ -0,0 +1,14 @@
+document.body.addEventListener("keypress", start);
+document.getElementById("btnShare").addEventListener("click", start);
+document.getElementById("btnRe").addEventListener("click", start);
+
+//for touch screen devices
+//document.getElementById("btnShare").addEventListener("touchstart", start);
+//document.getElementById("btnRe").addEventListener("touchstart", start);
+
+function start(event){
+	if(event.keyCode == undefined && event.target.id == 'btnRe' || event.keyCode == 110)
+		window.location = "index.html";
+	if(event.keyCode == undefined && event.target.id == 'btnShare' || event.keyCode == 121)
+		window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(urlShare),'facebook-share-dialog','width=626,height=436');
+}
