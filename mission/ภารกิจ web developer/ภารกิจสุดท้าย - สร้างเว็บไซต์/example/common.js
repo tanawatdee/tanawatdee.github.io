a728497d76073a84fc181ac1ef283d37d505aa22
tanawatdee <d.tanawat@hotmail.com>
2017-04-10 18:30:58 +0700

    add task5

--- mission/ภารกิจ web developer/ภารกิจสุดท้าย - สร้างเว็บไซต์/example/common.js
+++ mission/ภารกิจ web developer/ภารกิจสุดท้าย - สร้างเว็บไซต์/example/common.js
@@ -2,6 +2,10 @@ document.body.addEventListener("keypress", start);
 document.getElementById("btnYes").addEventListener("click", start);
 document.getElementById("btnNo").addEventListener("click", start);
 
+//for touch screen devices
+//document.getElementById("btnYes").addEventListener("touchstart", start);
+//document.getElementById("btnNo").addEventListener("touchstart", start);
+
 function start(event){
 	if(event.keyCode == undefined && event.target.id == 'btnNo' || event.keyCode == 110)
 		window.location = urlNo;
