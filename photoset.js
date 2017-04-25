b33316af4c901e97106ec60e6f6a10a13148b249
tanawatdee <d.tanawat@hotmail.com>
2017-04-26 00:15:21 +0700

    fix photoset.js

--- photoset.js
+++ photoset.js
@@ -1,7 +1,7 @@
 a=[],t='',p=100;
 $('.progress-bar').each(function(i, e){a.push($(e).attr('aria-valuenow'))});
-m=Math.min.apply(Math, a);
-for(i=2;!a.every((x)=>Math.abs(x%m)<m/p||Math.abs(m-x%m)<m/p);i++)m=m/i;
+m=M=Math.min.apply(Math, a);
+for(i=2;!a.every((x)=>Math.abs(x%m)<m/p||Math.abs(m-x%m)<m/p);i++)m=M/i;
 b=a.map((x)=>Math.round(x/m));
 s=b.reduce((a, b) => a + b, 0);
 for(i=0;i<a.length;i++){t+='<tr><td>'+a[i]+'</td><td>'+b[i]+'</td></tr>'}
