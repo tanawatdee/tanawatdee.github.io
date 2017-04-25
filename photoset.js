84254e06b0386c4ca5233e48468b2841fd932283
tanawatdee <d.tanawat@hotmail.com>
2017-04-25 23:42:27 +0700

    update photoset.js

--- photoset.js
+++ photoset.js
@@ -1,1 +1,8 @@
-$('.progress-bar').each(function(i, e){console.log($(e).attr('aria-valuenow'))});
+a=[],t='',p=100;
+$('.progress-bar').each(function(i, e){a.push($(e).attr('aria-valuenow'))});
+m=Math.min.apply(Math, a);
+for(;!a.every((x)=>Math.abs(x%m)<m/p||Math.abs(m-x%m)<m/p);m+=m);
+b=a.map((x)=>Math.round(x/m));
+s=b.reduce((a, b) => a + b, 0);
+for(i=0;i<a.length;i++){t+='<tr><td>'+a[i]+'</td><td>'+b[i]+'</td></tr>'}
+$('body').html('<table class="table table-striped"><tr><th>Percent</th><th>Possible sets</th></tr>'+t+'<tr><td colspan="2"><h3>total <b>i x '+s+' sets</b>, i is an integer.</h3></td></tr></table>');
