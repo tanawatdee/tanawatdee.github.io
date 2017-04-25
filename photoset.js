c75eb9dd05f13f56f2e4318e5c7c13b236bd44e0
tanawatdee <d.tanawat@hotmail.com>
2017-04-26 00:13:20 +0700

    edit photoset.js

--- photoset.js
+++ photoset.js
@@ -1,7 +1,7 @@
 a=[],t='',p=100;
 $('.progress-bar').each(function(i, e){a.push($(e).attr('aria-valuenow'))});
 m=Math.min.apply(Math, a);
-for(;!a.every((x)=>Math.abs(x%m)<m/p||Math.abs(m-x%m)<m/p);m+=m);
+for(i=2;!a.every((x)=>Math.abs(x%m)<m/p||Math.abs(m-x%m)<m/p);i++)m=m/i;
 b=a.map((x)=>Math.round(x/m));
 s=b.reduce((a, b) => a + b, 0);
 for(i=0;i<a.length;i++){t+='<tr><td>'+a[i]+'</td><td>'+b[i]+'</td></tr>'}
