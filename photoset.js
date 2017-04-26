5d5d1cd8ac37ac67966c094457e4b24d92d2de33
tanawatdee <d.tanawat@hotmail.com>
2017-04-26 16:02:40 +0700

    fix photoset/index

--- photoset.js
+++ photoset.js
@@ -1,8 +1,1 @@
-a=[],t='',p=100;
-$('.progress-bar').each(function(i, e){a.push($(e).attr('aria-valuenow'))});
-m=M=Math.min.apply(Math, a);
-for(i=2;!a.every((x)=>Math.abs(x%m)<m/p||Math.abs(m-x%m)<m/p);i++)m=M/i;
-b=a.map((x)=>Math.round(x/m));
-s=b.reduce((a, b) => a + b, 0);
-for(i=0;i<a.length;i++){t+='<tr><td>'+a[i]+'</td><td>'+b[i]+'</td></tr>'}
-$('body').html('<table class="table table-striped"><tr><th>Percent</th><th>Possible sets</th></tr>'+t+'<tr><td colspan="2"><h3>total <b>i x '+s+' sets</b>, i is an integer.</h3></td></tr></table>');
+window.location='//tanawatdee.github.io/photoset';
